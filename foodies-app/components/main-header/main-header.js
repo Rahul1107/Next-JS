4ed8ac6d206a82daa35dca7af526487343fdd33a
Rahul verma <rahulverma11071998@gmail.com>
2024-06-06 14:16:17 +0530

    Homepage and Nav Setup

--- foodies-app/components/main-header/main-header.js
+++ foodies-app/components/main-header/main-header.js
@@ -1,11 +1,16 @@
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
 import MainHeaderBackground from './main-header-background'
+import NavLink from './nav-link'
 
 const MainHeader = () => {
+
+
     return (
         <>
         <MainHeaderBackground/>
@@ -18,10 +23,10 @@ const MainHeader = () => {
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Browse Meals</Link>
+                        <NavLink href='/meals' >Browse Meals</NavLink>
                     </li>
                     <li>
-                        <Link href="/community">Foodies Community</Link>
+                        <NavLink href='/community' >Foodies Community</NavLink>
                     </li>
                 </ul>
             </nav>
