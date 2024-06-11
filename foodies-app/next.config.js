92b9a6a15f8e7f65af1208474f6dd2e6025c2728
Rahul verma <rahulverma11071998@gmail.com>
2024-06-11 20:44:53 +0530

    completed

--- foodies-app/next.config.js
+++ foodies-app/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'foodies-app-nextjs-users-image.s3.eu-north-1.amazonaws.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
